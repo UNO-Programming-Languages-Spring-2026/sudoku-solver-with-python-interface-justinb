4450c735755ea4d000b14b4a8dfa6c5bdb83d682
Justin Bartels <justinbartels@unomaha.edu>
2026-04-01 20:34:45 -0500

    question 1b completed

--- sudoku1.py
+++ sudoku1.py
@@ -2,6 +2,11 @@ import clingo
 
 class ClingoApp(clingo.application.Application):
 
+    def print_model(self, model, printer) -> None:
+        symbols = model.symbols(shown=True)
+        symbols = sorted(symbols)
+        print(" ".join(str(s) for s in symbols))
+
     def main(self, ctl, files):
         for f in files:
             ctl.load(f)
